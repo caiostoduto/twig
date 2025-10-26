65cc2d44fe26ab608e681a4c32bda0dada5a117f
Caio Stoduto <88816833+caiostoduto@users.noreply.github.com>
2025-10-26 20:53:52 -0300

    fix: remove unnecessary vec command check

--- src/commands/minecraft/mod.rs
+++ src/commands/minecraft/mod.rs
@@ -1,13 +1,12 @@
 mod uptime;
 
-use crate::{Context, Data, Error, utils::config};
+use crate::{Context, Data, Error};
 
 use uptime::*;
 
 /// Minecraft command group
 #[poise::command(
     slash_command,
-    // subcommands("list", "status"),
     subcommands("uptime"),
     subcommand_required = true,
     category = "Minecraft"
@@ -18,10 +17,5 @@ pub async fn minecraft(_ctx: Context<'_>) -> Result<(), Error> {
 
 /// Returns all bot commands related to Minecraft category
 pub fn commands() -> Vec<poise::Command<Data, Error>> {
-    // Only return Minecraft commands if all required config values are set
-    if config::get_config().docker_socket.is_some() {
-        vec![minecraft()]
-    } else {
-        vec![]
-    }
+    vec![minecraft()]
 }
